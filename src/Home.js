3606c252cfe191f539a5f0493559f25036849dda
BALASURYA S <139736173+BalasuryaMCA2023@users.noreply.github.com>
2024-08-27 20:18:11 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ export default function Home(){
     return(
         <>
         <div id="home">
-         <h3>Welcome to <span id="span-1">Union BANK</span>-<span id="span-2">ATM</span></h3>
+         <h3>Welcome to <span id="span-1">SBI BANK</span>-<span id="span-2">ATM</span></h3>
          <p> Available 24/7 from almost any device with an internet connection</p>
           <p>Create new Account ,balance checks and deposit your money.</p>
 
